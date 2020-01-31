ff06d182c6951516e1ce4f2abac34710a82fffd3
Andrea Bermudez <andreabermudez_bcnvj1ma1920@cev.com>
2020-01-31 10:12:56 +0100

    sdasd

--- Easings/Assets/Scripts/Player.cs
+++ Easings/Assets/Scripts/Player.cs
@@ -16,24 +16,83 @@ public class Player : MonoBehaviour
     }
     public Levels shotLevel;
 
+    ConstantForce fuerzaConstante;
+
+    private Vector2 screenBounds;
+
+    private void Start()
+    {
+        fuerzaConstante = GetComponent<ConstantForce>();
+        screenBounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, Camera.main.transform.position.z));
+        
+    }
+
     private void Update()
+    {
+        ApplyMovement();
+        Shot();
+        MargingSpace(); 
+    }
+
+    void MargingSpace()
+    {
+        if(transform.position.x >= screenBounds.x)
+        {
+           
+        }
+    }
+
+    void ApplyMovement()
+    {
+        Vector3 desiredForce = Vector3.zero;
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            desiredForce += new Vector3(-100f, 0f,0f);
+        }
+        else if (Input.GetKey(KeyCode.DownArrow))
+        {
+            desiredForce += new Vector3(100f, 0f, 0f);
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            desiredForce += new Vector3(0f, 100f, 0f);
+        }
+        else if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            desiredForce += new Vector3(0f, -100f, 0f);
+        }
+
+        fuerzaConstante.relativeForce = desiredForce;
+    }
+
+    void Shot()
     {
         if (Input.GetButton("Jump"))
         {
             switch (shotLevel)
             {
                 case Levels.level1:
-
+                    ActivateEmission(particlesLV1);
+                    DeaactivateEmission(particlesLV2);
+                    DeaactivateEmission(particlesLV3);
                     break;
                 case Levels.level2:
+                    ActivateEmission(particlesLV2);
+                    DeaactivateEmission(particlesLV1);
+                    DeaactivateEmission(particlesLV3);
                     break;
                 case Levels.level3:
+                    ActivateEmission(particlesLV3);
+                    DeaactivateEmission(particlesLV1);
+                    DeaactivateEmission(particlesLV2);
                     break;
             }
         }
         else
         {
-
+            DeaactivateEmission(particlesLV2);
+            DeaactivateEmission(particlesLV1);
+            DeaactivateEmission(particlesLV3);
         }
     }
 
